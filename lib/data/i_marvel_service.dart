d7b550be9cbabe44db317d2844383122094d08a6
yuri <kayel.yuri@gmail.com>
2024-08-27 14:01:03 -0300

    marvelous service

---
+++ lib/data/i_marvel_service.dart
@@ -0,0 +1,103 @@
+import "package:im_mottu_mobile/index.dart";
+
+abstract class IMarvelService {
+
+  String generateHash(String timestamp);
+
+  // Characters
+  Future<CharacterDataWrapper> fetchCharacters({
+    String? name,
+    String? nameStartsWith,
+    DateTime? modifiedSince,
+    List<int>? comics,
+    List<int>? series,
+    List<int>? events,
+    List<int>? stories,
+    String? orderBy,
+    int? limit,
+    int? offset,
+  });
+
+  // Comics
+  Future<ComicDataWrapper> fetchComics({
+    String? title,
+    String? titleStartsWith,
+    DateTime? modifiedSince,
+    List<int>? format,
+    List<int>? formatType,
+    bool? noVariants,
+    List<int>? dateDescriptor,
+    DateTime? dateRangeStart,
+    DateTime? dateRangeEnd,
+    bool? hasDigitalIssue,
+    List<int>? characters,
+    List<int>? series,
+    List<int>? events,
+    List<int>? creators,
+    List<int>? stories,
+    String? orderBy,
+    int? limit,
+    int? offset,
+  });
+
+  // Creators
+  Future<CreatorDataWrapper> fetchCreators({
+    String? firstName,
+    String? middleName,
+    String? lastName,
+    String? suffix,
+    DateTime? modifiedSince,
+    List<int>? comics,
+    List<int>? series,
+    List<int>? events,
+    List<int>? stories,
+    String? orderBy,
+    int? limit,
+    int? offset,
+  });
+
+  // Events
+  Future<EventDataWrapper> fetchEvents({
+    String? name,
+    String? nameStartsWith,
+    DateTime? modifiedSince,
+    List<int>? creators,
+    List<int>? characters,
+    List<int>? comics,
+    List<int>? series,
+    List<int>? stories,
+    String? orderBy,
+    int? limit,
+    int? offset,
+  });
+
+  // Series
+  Future<SeriesDataWrapper> fetchSeries({
+    String? title,
+    String? titleStartsWith,
+    DateTime? modifiedSince,
+    List<int>? comics,
+    List<int>? stories,
+    List<int>? events,
+    List<int>? creators,
+    List<int>? characters,
+    String? seriesType,
+    String? contains,
+    String? orderBy,
+    int? limit,
+    int? offset,
+  });
+
+  // Stories
+  Future<StoryDataWrapper> fetchStories({
+    DateTime? modifiedSince,
+    List<int>? comics,
+    List<int>? series,
+    List<int>? events,
+    List<int>? creators,
+    List<int>? characters,
+    String? orderBy,
+    int? limit,
+    int? offset,
+  });
+}
