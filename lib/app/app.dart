a71aa1ac5aaf111afa4cd1b3ef1b5e4b612bbaf7
yuri <kayel.yuri@gmail.com>
2024-09-01 11:44:23 -0300

    app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,14 +1,16 @@
-import "package:im_mottu_mobile/index.dart";
+import 'package:im_mottu_mobile/index.dart';
 
 class MarvelApp extends StatelessWidget {
   const MarvelApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Marvel Characters',
       theme: ThemeData(primarySwatch: Colors.red),
-      home: const CharacterListScreen(),
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+      initialBinding: MarvelBindings.instance,
     );
   }
 }
