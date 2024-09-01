1e35145dbfd0d691f23d3af061361eb8fb9312ff
yuri <kayel.yuri@gmail.com>
2024-09-01 11:46:40 -0300

    index

--- lib/app/home.dart
+++ lib/app/home.dart
@@ -1,46 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:im_mottu_mobile/index.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return FadeTransition(
+      opacity: _animation,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Marvel API Explorer'),
+          titleTextStyle: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+            color: Colors.black,
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.yellow,
+        ),
+        body: Container(
+          color: Colors.black,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  final crossAxisCount = constraints.maxWidth > 1200
+                      ? 4
+                      : constraints.maxWidth > 800
+                          ? 3
+                          : 2;
+
+                  return GridView.count(
+                    crossAxisCount: crossAxisCount,
+                    crossAxisSpacing: 16.0,
+                    mainAxisSpacing: 16.0,
+                    shrinkWrap: true,
+                    children: [
+                      _buildCategoryButton(
+                        context,
+                        'Character',
+                        Icons.person,
+                        CharacterListScreen(
+                          characterViewModel: Get.find<CharacterViewModel>(),
+                        ),
+                      ),
+                      _buildCategoryButton(
+                        context,
+                        'Comic',
+                        Icons.book,
+                        ComicListScreen(
+                          comicViewModel: Get.find<ComicViewModel>(),
+                        ),
+                      ),
+                      _buildCategoryButton(
+                        context,
+                        'Creator',
+                        Icons.create,
+                        CreatorListScreen(
+                          creatorViewModel: Get.find<CreatorViewModel>(),
+                        ),
+                      ),
+                      _buildCategoryButton(
+                        context,
+                        'Event',
+                        Icons.event,
+                        EventListScreen(
+                          eventViewModel: Get.find<EventViewModel>(),
+                        ),
+                      ),
+                      _buildCategoryButton(
+                        context,
+                        'Series',
+                        Icons.tv,
+                        SeriesListScreen(
+                          seriesViewModel: Get.find<SeriesViewModel>(),
+                        ),
+                      ),
+                      _buildCategoryButton(
+                        context,
+                        'Story',
+                        Icons.library_books,
+                        StoryListScreen(
+                          storyViewModel: Get.find<StoryViewModel>(),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
             ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+
+  Widget _buildCategoryButton(
+    BuildContext context,
+    String title,
+    IconData icon,
+    Widget destinationScreen,
+  ) {
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => destinationScreen),
+        );
+      },
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.all(16.0),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.red[900],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 40.0, color: Colors.red[900]),
+          const SizedBox(height: 8.0),
+          Text(
+            title,
+            style: TextStyle(fontSize: 18.0, color: Colors.red[900]),
+          ),
+        ],
       ),
     );
   }
--- lib/index.dart
+++ lib/index.dart
@@ -1,17 +1,7 @@
-
-// DART
-export 'dart:convert';
-export 'dart:async';
-export 'dart:io';
-
-// FLUTTER
-export 'package:crypto/crypto.dart';
-export "package:flutter/foundation.dart";
-export 'package:flutter/material.dart';
-
 // APP
 export 'app/app.dart';
-export 'app/home.dart';
+export "app/home.dart";
+export "app/splash.dart";
 
 // MODEL
 export 'model/character.dart';
@@ -21,17 +11,48 @@ export 'model/event.dart';
 export 'model/series.dart';
 export 'model/story.dart';
 
-// DATA
-export 'data/i_marvel_service.dart';
-export 'data/marvel_service.dart';
+// VIEW
+export 'view/character_list_screen.dart';
+export 'view/character_detail_screen.dart';
+export 'view/comic_list_screen.dart';
+export 'view/comic_detail_screen.dart';
+export 'view/creator_list_screen.dart';
+export 'view/creator_detail_screen.dart';
+export 'view/event_list_screen.dart';
+export 'view/event_detail_screen.dart';
+export 'view/series_list_screen.dart';
+export 'view/series_detail_screen.dart';
+export 'view/story_list_screen.dart';
+export 'view/story_detail_screen.dart';
 
-// ROUTES
-export 'routes/character_list_screen.dart';
-export 'routes/character_detail_screen.dart';
+// VIEWMODEL
+export 'viewmodel/character_viewmodel.dart';
+export 'viewmodel/comic_viewmodel.dart';
+export 'viewmodel/creator_viewmodel.dart';
+export 'viewmodel/event_viewmodel.dart';
+export 'viewmodel/series_viewmodel.dart';
+export 'viewmodel/story_viewmodel.dart';
+
+// DEPENDENCY INJECTION
+export 'app/bindings.dart';
+
+// DATA
+export 'data/i_marvel_repository.dart';
+export 'data/marvel_repository.dart';
+export 'data/i_marvel_remote.dart';
 
 // PUB.DEV
-export "package:dio/dio.dart";
+export "package:get/get.dart";
 export "package:dio_cache_interceptor/dio_cache_interceptor.dart";
 export "package:pretty_dio_logger/pretty_dio_logger.dart";
 export 'package:cached_network_image/cached_network_image.dart';
 export 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// DART
+export 'dart:convert';
+export 'dart:async';
+export 'package:crypto/crypto.dart';
+
+// FLUTTER
+export "package:flutter/foundation.dart";
+export 'package:flutter/material.dart';
